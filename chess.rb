7e26a1566cc83f1bf5a25b320720bf30ffe9be28
Morgan Waddington <waddington.morgan@gmail.com>
2023-01-31 18:11:22 -0500

    final

--- chess.rb
+++ chess.rb
@@ -1,11 +1,16 @@
 class Employee
-
+    
+    attr_reader :name, :salary, :boss
 
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
+        
+        if boss != nil
+            @boss.employees << self
+        end
     end
 
 
@@ -20,7 +25,7 @@ end
 
 class Manager < Employee
 
-    attr_reader :name
+    attr_reader :employees
 
     def initialize(name, title, salary, boss)
         super
@@ -28,7 +33,9 @@ class Manager < Employee
     end 
 
     def add_employees(employee)
-        @employees << employee
+        # if employee.boss == @name
+            @employees << employee
+        # end
     end
 
     def bonus(multiplier)
@@ -36,12 +43,33 @@ class Manager < Employee
     end
 
     def total_salary
-        
+        sum = 0
+        @employees.each do |ele|
+            if !ele.is_a?(Manager)
+                sum += ele.salary
+            else
+                output = ele.total_salary + ele.salary
+                sum += output
+            end
+        end
+        sum
     end
-
 
 end
 
+
+p ned = Manager.new('Ned', 'Founder', 1000000, nil)
+p darren = Manager.new('Darren', 'TA Manager', 78000, ned )
+p david = Employee.new('David', 'TA', 10000, darren )
+p shawna = Employee.new('Shawna', 'TA', 12000, darren )
+
+# darren.add_employees(david = Employee.new('David', 'TA', 10000, 'Darren' ))
+# darren.add_employees(shawna = Employee.new('Shawna', 'TA', 12000, 'Darren' ))
+
+
+p david.bonus(3)
+p ned.bonus(5)
+p darren.bonus(4)
 
 
 
